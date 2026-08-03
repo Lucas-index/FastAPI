f86197348eacaf0d11da43f28d9c3b0db1576d08
Lucas-Index <lucasfaria201045@gmail.com>
2026-08-03 14:12:02 -0300

    Alterações adicionais

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter #biblioteca APIRouter
+from sqlalchemy import Engine
 from sqlalchemy.orm import sessionmaker # type: ignore #biblioteca sessionmaker
 from models import Usuario, db #importa a classe Usuario do arquivo models.py
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"]) #definindo a variavel auth_router como APIRouter
@@ -13,13 +14,13 @@ async def autenticação():
 
 @auth_router.post("/login") #define a rota de login
 async def entrada(email: str, senha: str): #mostra que str é string mais de uma forma simplificada
-    Session = sessionmaker(bind=engine) #cria uma sessão de banco de dados
+    Session = sessionmaker(bind=Engine) #cria uma sessão de banco de dados
     session = Session() #cria uma sessão de banco de dados
     usuario = session.query(Usuario).filter(Usuario.email == email, Usuario.senha == senha).first() #faz uma consulta no banco de dados para verificar se o usuário existe e filtra pelo email e senha
     if usuario:
         return {"message": "Login realizado com sucesso", "autenticado": True}
     else:
         novo_usuario = Usuario(email=email, senha=senha) #cria um novo usuário
-        session.add(novo_usuario)
+        session.add(novo_usuario) 
         session.commit()
         return {"message": "Usuário criado com sucesso", "autenticado": False}
